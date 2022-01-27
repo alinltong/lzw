cde471bd83e536ab4023fe565f661ae5ae719a0e
alinltong <2188014893@qq.com>
2022-01-27 15:29:57 +0800

    dh

---
+++ cdd/.vscode/lain.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+	int num;
+	cout<<"请输入一个整型数：";
+	cin>>num;
+	cout<<"八进制数是："<<oct<<num<<endl;
+	cout<<"十进制数是："<<dec<<num<<endl;
+	cout<<"十六进制数是："<<hex<<num<<endl;
+	return 0;
+}
